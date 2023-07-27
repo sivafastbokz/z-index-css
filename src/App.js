83f9c8a8e4b1907bc1f913775c9391771bec88e8
sivafastbokz <sivaharshanfastbokz@gmail.com>
2023-07-27 19:53:44 +0530

    some updates pending

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-   <React.Fragment>
-    <div className='box1'>box1</div>
-    <input type='number'></input>
+  const [zIndex, setZIndex] = useState(0);
 
-    <div className='box2'>box1</div>
-    <input type='number'></input>
-    
-    <div className='box2'>box1</div>
-    <input type='number'></input>
-   </React.Fragment>
-   
+  const handleChangeZIndex = (event) => {
+    const value = parseInt(event.target.value);
+      setZIndex(value);
+  };
+
+  return (
+    <React.Fragment>
+      <div className={` box1 ${zIndex <= 3 ? 'front' : ''}`}>box1</div>
+      <input type='number' placeholder='box1' onChange={handleChangeZIndex}></input>
+
+      <div className={` box2 ${zIndex <= 3 ? 'front' : ''}`}>box2</div>
+      <input type='number' placeholder='box2' onChange={handleChangeZIndex}></input>
+
+      <div className={` box3 ${zIndex <= 3 ? 'front' : ''}`}>box3</div>
+      <input type='number' placeholder='box3' onChange={handleChangeZIndex}></input>
+    </React.Fragment>
   );
 }
 
